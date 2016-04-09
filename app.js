17dd095a358277657f4a46c5d14cc1213048f12b
Hannes Kleist <hannes@stanwood.de>
2016-04-09 09:56:18 +0200

    Fixed links, added multiple emails

--- app.js
+++ app.js
@@ -6,13 +6,29 @@ function sent(form) {
     var guest = new Guest();
 
     var namePlusOne = "";
+    var emailPlusOne = "";
     $(".morePersons").each(function() {
         namePlusOne += $(this)[0].value + ", ";
     });
+    $(".moreEmails").each(function() {
+        emailPlusOne += $(this)[0].value + ", ";
+    });
+
+    if (form.name.value == "" || form.email.value == "") {
+        alert("Bitte gib Deine E-Mail und Deinen Namen an.");
+        return;
+    }
+
+    if (form.comesToCeremony.checked == false && form.comesToParty.checked == false && form.doesNotCome.checked == false) {
+        alert("Bitte sage uns, ob Du kommst oder nicht.");
+        return
+    }
 
     guest.save({
         name: form.name.value,
+        email: form.email.value,
         namePlusOne: namePlusOne,
+        emailPlusOne: emailPlusOne,
         comesToCeremony: form.comesToCeremony.checked,
         comesToParty: form.comesToParty.checked,
         doesNotCome: form.doesNotCome.checked,
@@ -32,7 +48,9 @@ var numberOfMoreGuests = 0;
 
 function addPerson(person) {
 
-    $(person).before('<input class="morePersons" placeholder="Vorname Nachname" name="name' + numberOfMoreGuests + '" style="width: 80%;">');
+    $(person).before('<input class="moreEmails" placeholder="E-Mail" name="email' +
+        numberOfMoreGuests + '" style="width: 80%;"><input class="morePersons" placeholder="Vorname Nachname" name="name' +
+        numberOfMoreGuests + '" style="width: 80%;">');
     numberOfMoreGuests++;
 
     if (numberOfMoreGuests > 5) {
